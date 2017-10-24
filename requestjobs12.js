94d935730ff75394d577b4af459bd505b565c65a
ASWANDI <33065018+onjobs@users.noreply.github.com>
2017-10-24 23:54:56 +0700

    Add files via upload

--- requestjobs12.js
+++ requestjobs12.js
@@ -33,7 +33,7 @@ if(x=="8"){xxx="/2017/10/10-tips-to-get-cheapest-car-insurance.html"}
 if(x=="9"){xxx="/2017/10/5-things-to-consider-before-selling.html"}
 if(x=="10"){xxx="/2017/10/sell-your-future-annuity-payments-for.html"}
 if(x=="11"){xxx="/2017/10/8-tips-to-help-you-before-donate-car-to.html"}
-if(x=="12"){xxx="/2017/10/mesothelioma-and-asbestos-lawyers.html"}a_to_ve[i].href="https://infomylink.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="12"){xxx="/2017/10/mesothelioma-and-asbestos-lawyers.html"}a_to_ve[i].href="http://online-jobs.cf"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
